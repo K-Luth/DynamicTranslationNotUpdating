5f7e61a9dd9f4f95dc73e54296f0d5a4dadb8044
Robbert Slager <robbert.slager@gain.nl>
2021-10-14 07:45:34 +0200

    Add project files.

---
+++ TestAppForMVVMwithBaseClasses/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TestAppForMVVMwithBaseClasses.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
